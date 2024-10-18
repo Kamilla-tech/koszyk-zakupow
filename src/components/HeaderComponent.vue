aafdcf144fd6b0c02ab1d47d10bdbc3a08e4c4cf
Kamila Rembisz-Koszkul <kamila.rembisz@gmail.com>
2024-10-18 11:45:59 +0200

    dodanie w template header, span oraz button. Dodane w script export default oraz dodanie w style styli.

---
+++ src/components/HeaderComponent.vue
@@ -0,0 +1,33 @@
+<template>
+  <header class="container">
+    <span class="count">
+      Ilość produktów w koszyku:
+      {{ itemCount}}
+    </span>
+    <button @click="$emit('toggle-cart')">&#12872;2</button>
+  </header>
+</template>
+
+<script>
+  export default {
+    props: {
+      itemCount: {
+        type: Number,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .container {
+    display: flex;
+    justify-content: flex-end;
+    align-items: flex-end;
+  }
+  
+  .count {
+    margin-right: 10px;
+    color: #333;
+  }
+</style>
