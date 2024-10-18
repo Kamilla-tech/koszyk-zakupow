94b3d8d1afd53c9dc4b3336771cdc137f13b9a3f
Kamila Rembisz-Koszkul <kamila.rembisz@gmail.com>
2024-10-18 11:50:52 +0200

    utowrzenie w template section, h3 oraz ul. Zaimportowanie w script ProductTitle oraz dodanie komponentu i props.

---
+++ src/components/ProductList.vue
@@ -0,0 +1,23 @@
+<template>
+  <section class="container">
+    <h3>Lista produktów w koszyku:</h3>
+    <ul class="products">
+      
+    </ul>
+  </section>
+</template>
+
+<script>
+  import ProductTitle from "./ProductTitle.vue";
+  
+  export default {
+    components: {
+      ProductTitle
+    },
+    props: ["products"]
+  }
+</script>
+
+<style scoped>
+
+</style>
