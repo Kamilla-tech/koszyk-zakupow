84e056889eeb02170a20e12dd317dc08ababff34
Kamila Rembisz-Koszkul <kamila.rembisz@gmail.com>
2024-11-23 17:37:58 +0100

    utworzenie listy produktow

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -2,7 +2,13 @@
   <section class="container">
     <h3>Lista produktów w koszyku:</h3>
     <ul class="products">
-      
+      <ProductTitle
+          v-for="product in products"
+          :key="product.id"
+          :product="product"
+          @update-quantity="$emit('update-quantity', $event)"
+          @remove-product="$emit('remove-product', product.id)"
+      />
     </ul>
   </section>
 </template>
@@ -19,5 +25,11 @@
 </script>
 
 <style scoped>
-
+  .products {
+    list-style-type: none;
+    padding: 0 5px 0 0;
+    margin: 0;
+    max-height: 300px;
+    overflow-y: auto;
+  }
 </style>
