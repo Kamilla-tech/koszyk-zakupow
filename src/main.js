6cffe1679fe9167beef7992be806f023a3975ad0
Kamila Rembisz-Koszkul <kamila.rembisz@gmail.com>
2025-01-20 12:30:52 +0100

    Use Font Awesome icon for remove button
    The remove button in ProductTitle.vue now uses a Font Awesome "xmark" icon instead of plain text. Updated main.js to include and register the required Font Awesome library and icon.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { library } from '@fortawesome/fontawesome-svg-core';
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
+library.add(faXmark);
 
 const app = createApp(App);
 app.component('font-awesome-icon', FontAwesomeIcon);
